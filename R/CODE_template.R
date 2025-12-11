c24a885ecd9aaf6d3386c46e8862ef4ae01fc268
Johan van Tent <tentvanjohan@hotmail.com>
2025-12-11 16:17:14 +0100

    toevoeging van code dummy file aan template

---
+++ R/CODE_template.R
@@ -0,0 +1,34 @@
+# library(HHSKwkl)
+# library(tidyverse)
+# library(glue)
+# 
+# theme_set(hhskthema())
+
+# library(readxl)
+# library(openxlsx)
+# library(sf)
+# library(leaflet)
+
+print("Code voor template")
+
+# fys_chem <- data_online("fys_chem.rds")
+# meetpunten <- data_online("meetpunten.rds")
+# parameters <- data_online("parameters.rds")
+# normen <- data_online("normen.rds")
+# biologie <- data_online("biologie.rds")
+
+# biologie <- 
+#   biologie %>% 
+#   select(-contains("stadium")) %>% 
+#   distinct()
+
+# ws_grens_wgs %>% 
+#   leaflet::leaflet() %>% 
+#   leaflet::addProviderTiles("CartoDB.Positron", group = "Kaart") %>%
+#   leaflet::addProviderTiles("Esri.WorldImagery", group = "Luchtfoto") %>% 
+#   leaflet::addLayersControl( baseGroups = c("Kaart", "Luchtfoto"), 
+#                              options = leaflet::layersControlOptions(collapsed = FALSE),
+#                              position = "topleft") %>% 
+#   leaflet::addPolylines(label = ~"waterschapsgrens", color = "grey", opacity = 1, weight = 3)
+
+
